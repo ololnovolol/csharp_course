3fb2e20b278499f0952b1f0a7a3f0f404f2fc1a6
ololnovaolol <novakoleg107@gmail.com>
2021-10-08 21:19:26 +0300

    solution setup

---
+++ Solution/Solution.Capture1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Solution.Capture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+        
+    }
+}
