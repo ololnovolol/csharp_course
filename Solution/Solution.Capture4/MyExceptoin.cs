9f249e459facc23633e5f116b0d2d8af5c750e41
ololnovaolol <novakoleg107@gmail.com>
2021-10-18 20:13:56 +0300

    Solution Capture Four finish =)

---
+++ Solution/Solution.Capture4/MyExceptoin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Capture4
+{
+    class MyExceptoin : ArgumentException
+    {
+        int value;
+        public MyExceptoin()
+        {
+            string g = base.Message;
+
+
+        }
+
+        public MyExceptoin(string message) : base(message)
+        {
+            string g = base.Message;
+            Console.WriteLine(g + " <--output inside MyExceptoin ctor");
+
+        }
+
+        public MyExceptoin(string message, int value) : base(message)
+        {
+            string g = base.Message;
+            Console.WriteLine(g + " <--output inside MyExceptoin ctor value =" + value);
+            this.value = value;
+        }
+    }
+}
