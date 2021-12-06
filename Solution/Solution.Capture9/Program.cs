55e2d03e0f56c47628cdcaede10309e11d4884f3
Олег Новак <novakoleg107@gmail.com>
2021-12-06 16:58:18 +0200

    Capture nine is started

--- Solution/Solution.Capture9/Program.cs
+++ Solution/Solution.Capture9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Solution.Capture9
 {
@@ -7,7 +8,8 @@ namespace Solution.Capture9
     {
         static void Main(string[] args)
         {
-            ArrayListMethod();
+            //ArrayListMethod();
+            ListMethod();
 
 
         }
@@ -24,12 +26,25 @@ namespace Solution.Capture9
             {
                 Console.WriteLine(item);
             }
-            var type0ik = list[0];
-            var type1ik = list[1];
-            var type2ik = list[2];
+            var type0ik =list[3];
+            var type1ik = list[4];
+            var type2ik = list[5];
             Console.WriteLine("\tzero = " + type0ik.ToString() + "\n\tzero type = " + type0ik.GetType());
             Console.WriteLine("\tone = " + type1ik.ToString() + "\n\tone type = " + type1ik.GetType());
             Console.WriteLine("\ttwo = " + type2ik.ToString() + "\n\tztwo type = " + type2ik.GetType());
+        }
+        public static void ListMethod()
+        {
+            List<int> numbers = new() { 0, 1, 2, 3, 4, 5 };
+           
+            numbers.AddRange(new int[] { 6, 7, 8, 9, 10 });
+
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
