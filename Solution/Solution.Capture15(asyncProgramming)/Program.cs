6e7dc3b6347458b1710061aa2f4e8abe9828e412
Временный <@DESKTOP-Q4PJU9S>
2022-04-02 23:58:00 +0300

    async 40% learning

--- Solution/Solution.Capture15(asyncProgramming)/Program.cs
+++ Solution/Solution.Capture15(asyncProgramming)/Program.cs
@@ -10,9 +10,16 @@ namespace Solution.Capture15_asyncProgramming_
         {
             //BaseExampleVol1Async();
             //BaseExampleVol2Async();
-
+            //await LyambdaAsynkExample();
+            //await ReturnRezultVoidAsync();
+            //await ReturnRezultTaskAsync();
+            //await ReturnRezultTaskAsyncVol1();
+            //await ReturnRezultTaskTAsyncVol2();
+            //await ReturnRezultTaskTAsyncVol3();
+            await ReturnRezultVALUETaskTisSTRUCTAsync();
 
             Console.ReadKey();
+
 
         }
         public async static void BaseExampleVol1Async()
@@ -37,7 +44,6 @@ namespace Solution.Capture15_asyncProgramming_
                 Console.WriteLine("Конец метода PrintAsync");
             }
         }
-
         public async static void BaseExampleVol2Async()
         {
             //вызываю три способа одной задачи для понимания оптимизации Асинхрона
@@ -74,7 +80,7 @@ namespace Solution.Capture15_asyncProgramming_
             }
             async Task PRINTvar2OPTIMIZATIONasync()
             {
-                
+
                 var start1 = Print("async OPTIMIZED =)");
                 var start2 = Print("async OPTIMIZED =)");
                 var start3 = Print("async OPTIMIZED =)");
@@ -91,8 +97,122 @@ namespace Solution.Capture15_asyncProgramming_
                 }
             }
         }
+        public async static Task LyambdaAsynkExample()
+        {
+
+            Func<string, Task> Print = async (message) =>
+            {
+                await Task.Delay(3000);
+                Console.WriteLine(message);
+            };
+
+            await Print("one");
+            await Print("two");
+            await Print("three");
+        }
+
+        public async static Task ReturnRezultVoidAsync()
+        {
+            VoidReturnExample voidRE = new VoidReturnExample();
+            await voidRE.Starter();
+
+        }
+        public async static Task ReturnRezultTaskAsync()
+        {
+            var task = PrintAsync("one - one");
+            Console.WriteLine("Main Works");
+
+            await task; // task.Wait(); одно и тоже??
+
+            Console.WriteLine("Main end");
 
 
+            async Task PrintAsync(string message)
+            {
+                await Task.Delay(1000);
+                Console.WriteLine(message);
+                await Task.Delay(250);
+            }
+        }
+        public async static Task ReturnRezultTaskAsyncVol1()
+        {
+            int n1 = await SquareAsync(5);
+            int n2 = await SquareAsync(6);
+            Console.WriteLine($"n1={n1}  n2={n2}"); // n1=25  n2=36
 
-    }
+            async Task<int> SquareAsync(int n)
+            {
+                await Task.Delay(0);
+                return n * n;
+            }
+        }
+        public async static Task ReturnRezultTaskTAsyncVol2()
+        {
+            Person person = await GetPersonAsync("Tom");
+            Console.WriteLine(person.name); // Tom
+                                            // определение асинхронного метода
+            async Task<Person> GetPersonAsync(string name)
+            {
+                await Task.Delay(100);
+                return new Person(name);
+            }
+        }
+        public async static Task ReturnRezultTaskTAsyncVol3()
+        {
+            Task<int> task1 = Multiple(3);
+            var task2 = Multiple(5);
+
+            Console.WriteLine("procces main ");
+
+            int one = await task1;
+            int two = await task2;
+
+            Console.WriteLine($"one = {one}\n two = {two}");
+            //Console.WriteLine($"task1 type={task1.GetType()} \ntask2 type ={task2.GetType()} ");
+
+            async Task<int> Multiple(int number)
+            {
+                await Task.Delay(0);
+                int result = number * number;
+                Console.WriteLine($"result multiple: {result} = {number} * {number}");
+                return result;
+            }
+        }
+        public async static Task ReturnRezultVALUETaskTisSTRUCTAsync()
+        {
+            //await PrintAsync("Hello METANIT.COM");
+
+            //Person person = await GetPersonAsync("Tom");
+            //Console.WriteLine(person.name); // Tom
+
+            //// определение асинхронного метода
+            //async ValueTask PrintAsync(string message)
+            //{
+            //    await Task.Delay(0);
+            //    Console.WriteLine(message);
+            //}
+
+            //// определение асинхронного метода
+            //async ValueTask<Person> GetPersonAsync(string name)
+            //{
+            //    await Task.Delay(0);
+            //    return new Person(name);
+            //}
+            //record class Person(string Name);
+
+            ///данный код работает в новых версиях c# начиная с 9.0 вроде
+        }
+
+
+        public class Person
+        {
+            public string name;
+            public Person(string name)
+            {
+                this.name = name;
+            }
+
+        }
+
+    } 
 }
---
+++ Solution/Solution.Capture15(asyncProgramming)/VoidReturnExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Solution.Capture15_asyncProgramming_
+{
+    class VoidReturnExample
+    {
+
+        public async Task Starter()
+        {
+            Account acc = new Account();
+            acc.Added += PrintAsync;
+
+            acc.Put(500);
+
+            await Task.Delay(1000);
+
+            async void PrintAsync(object o, string message)
+            {
+                Console.WriteLine("Method PrintAsync started");
+                await Task.Delay(1000);
+                Console.WriteLine(message);
+            }
+        }
+
+        }
+    class Account
+    {
+        public int sum;
+        public event EventHandler<string> Added;
+
+        public void Put(int sum)
+        {
+            this.sum = sum;
+            Added?.Invoke(this, $"in count put {sum}");
+
+        }
+    }
+}
