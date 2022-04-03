40e0687336f533226d8e8680d19c76efc0b63c19
Временный <@DESKTOP-Q4PJU9S>
2022-04-03 20:10:19 +0300

    LINQ started

---
+++ Solution/Solution.Capture16(LINQ)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Capture16_LINQ_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
