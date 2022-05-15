05316574c415f61376cfa97a3068ef50349cb0a2
Олег Новак <novakoleg107@gmail.com>
2022-05-16 01:09:44 +0300

    ill be back=)

--- Solution/Solution.ASPnetCore.HelloApp/Program.cs
+++ Solution/Solution.ASPnetCore.HelloApp/Program.cs
@@ -5,23 +5,23 @@ using Microsoft.Extensions.FileProviders;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");  // call base hello world
 
-//app.UseWelcomePage(); // подключение к мидлвару WelcomePageMiddleware
-
-//int x = 2;
+//BaseMidlewareHelloASPnet(); // call base asp midleware world
 
 //app.Run(async (myContext) => await myContext.Response.WriteAsync("dcc"));
-// or
-//app.Run(HandleRequest);
+////or
+//app.Run(HandleRequest0); // call base my string
 
-
+//app.Run(HandleRequest1);
+//app.Run(HandleRequest2);
+//app.Run(HandleRequest3);
 
 //// получение пути запроса https://localhost:7094/next/daily/hello
 //app.Run(async (context) => await context.Response.WriteAsync
-//($"Path: {context.Request.Path}")); 
+//($"Path: {context.Request.Path}"));
 
-//или так https://localhost:7256/users?name=Tom&age=37
+////или так https://localhost:7256/users?name=Tom&age=37
 //app.Run(async (context) =>
 //{
 //    context.Response.ContentType = "text/html; charset=utf-8";
@@ -31,29 +31,31 @@ app.MapGet("/", () => "Hello World!");
 //        $"<p>QueryString: {context.Request.QueryString}</p>");
 //});
 
-
-
 //app.Run(HandleResponse);
-// получение пути запроса
+
+////получение пути запроса
 //app.Run(async (context) => await context.Response.WriteAsync($"Path: {context.Request.Path}"));
-// взаимодействие с путями
+
+////взаимодействие с путями
 //app.Run(HandlePathResponse);
-//
+
 //app.Run(HandleQueryvar1);
-//
+
 //app.Run(HandleQueryvar2);
-//
+
 //app.Run(HandleQueryvar3);
-// run FileImage
+
+////run FileImage
 //app.Run(async (context) =>
 //    await context.Response.SendFileAsync("pictures/iAmPicture.jpg"));
-// Run HtmlFile
+
+//Run HtmlFile
 //app.Run(async (context) =>
 //{
 //    var path = context.Request.Path;
 //    var fullPath = $"Html/{path}";
+//    //var fullPath = "Html//";
 //    var response = context.Response;
-
 
 //    response.ContentType = "text/html; charset=utf-8";
 
@@ -71,7 +73,8 @@ app.MapGet("/", () => "Hello World!");
 //    }
 
 //});
-// автоскачка файла
+
+//автоскачка файла
 //app.Run(async (context) =>
 //{
 //    context.Response.Headers.ContentDisposition = "attachment; filename=my_picture.jpg";
@@ -79,6 +82,7 @@ app.MapGet("/", () => "Hello World!");
 
 //});
 
+// автоскачка файла только путь приходит не в текстовом формате, а путь передается в виде обьекта IFieInfo
 //app.Run(async (context) =>
 //{
 //    var fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
@@ -100,7 +104,11 @@ app.MapGet("/", () => "Hello World!");
 //        string age = form["age"];
 //        string[] languages = form["languages"];
 
-//        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p><p>Languages:{languages[0]}{languages[1]}{languages[2]}</p></div>");
+//        await context.Response.WriteAsync($"<div>" +
+//                            $"<p>Name: {name}</p>" +
+//                            $"<p>Age: {age}</p>" +
+//                            $"<p>Languages:{languages[0]}{languages[1]}{languages[2]}</p>" +
+//                            $"</div>");
 //    }
 //    else
 //    {
@@ -126,7 +134,10 @@ app.MapGet("/", () => "Hello World!");
 //            langs += " [" + item + "]";
 //        }
 
-//        await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p><p>Languages:{langs}</p></div>");
+//        await context.Response.WriteAsync($"<div>" +
+//            $"<p>Name: {name}</p>" +
+//            $"<p>Age: {age}</p>" +
+//            $"<p>Languages:{langs}</p></div>");
 //    }
 //    else
 //    {
@@ -134,138 +145,139 @@ app.MapGet("/", () => "Hello World!");
 //    }
 //});
 
-//app.Run(async (context) =>
-//{
-//    if (context.Request.Path == "/old")
-//    {
-//        context.Response.Redirect("/new");
-//        context.Response.Redirect("https://z2.fm/");
-//    }
-//    else if(context.Request.Path == "/new")
-//    {
-//        await context.Response.WriteAsync("New Page");
-//    }
-//    else
-//    {
-//        await context.Response.WriteAsync("Main page");
-//    }
+app.Run(async (context) =>
+{
+    if (context.Request.Path == "/old")
+    {
+        //context.Response.Redirect("/new");
+        context.Response.Redirect("https://z2.fm/");
+    }
+    else if (context.Request.Path == "/new")
+    {
+        await context.Response.SendFileAsync("html/index.html");
+    }
+    else
+    {
+        await context.Response.WriteAsync("<p><a href=https://localhost:7094/new >/new</ a ></ p > " +
+            "<p><a href=https://localhost:7094/old>/old</a></p>" +
+            "<p><a href=https://localhost:7094/main>/this main</a></p>");
+    }
 
-//});
-
-
-
-
+});
 
 app.Run();
 
+#region HandleRequests
 
+void BaseMidlewareHelloASPnet()
+{
+    app.UseWelcomePage(); // подключение к мидлвару WelcomePageMiddleware
+}
 
+async Task HandleRequest0(HttpContext context)
+{
+    await context.Response.WriteAsync("hi hi");
+}
 
+async Task HandleRequest1(HttpContext context)
+{
+    int x = 2;
+    x = x * 2;
+    await context.Response.WriteAsync($"result 2 * 2 = {x}", System.Text.Encoding.Default);
+}
 
+async Task HandleRequest2(HttpContext context)
+{
+    var response = context.Response;
+    //response.StatusCode = 404;  // Установка кодов статуса
+    response.Headers.ContentLanguage = "ru=RU";
+    response.Headers.ContentType = "text/plain; charset=utf-8"; // для вывода кирилицы
+    response.Headers.Append("secret-id", "256"); // добавление кастомного заголовка
+    await response.WriteAsync("моя проэкт лежит на GitHub =)");
+}
 
+async Task HandleRequest3(HttpContext context)
+{
+    var response = context.Response;
+    response.Headers.ContentType = "text/html; charset=utf-8"; // для вывода HTML
+    await response.WriteAsync("<h2>Воробушек=)</h2>" +
+        "<h3>Welcome to web site vorobushek.com </h3>");
+}
 
+async Task HandleResponse(HttpContext context)
+{
+    var response = context.Response;
+    response.ContentType = "text/html; charset=utf-8";
+    var stringBuilder = new System.Text.StringBuilder("<table>");
 
+    foreach (var header in context.Request.Headers)
+    {
+        stringBuilder.Append($"<tr>" +
+            $"<td>{header.Key}</td>" +
+            $"<td>{header.Value}</td>" +
+            $"</tr>");
+    }
+    stringBuilder.Append("</table>");
+    await context.Response.WriteAsync(stringBuilder.ToString());
+}
 
-//async Task HandleRequest(HttpContext context)
-//{
-//    await context.Response.WriteAsync("hi hi");
-//}
+async Task HandlePathResponse(HttpContext context)
+{
+    var response = context.Response;
+    var path = context.Request.Path;
+    var now = DateTime.Now;
 
-//async Task HandleRequest(HttpContext context)
-//{
+    if (path == "/data")
+    {
+        await response.WriteAsync($"Date: {now.ToShortDateString()}");
+    }
+    else if (path == "/time")
+    {
+        await response.WriteAsync($"Time: {now.ToShortTimeString()}");
+    }
+    else if (path == "/minute")
+    {
+        await response.WriteAsync($"Minute: {now.ToString()}");
+    }
+    else
+    {
+        await response.WriteAsync($"\tdata => https://localhost:7094/data \n\thours => https://localhost:7094/time  \n\tminutes => https://localhost:7094/minute");
 
-//    x = x * 2;
-//    await context.Response.WriteAsync($"result 2 * 2 = {x}", System.Text.Encoding.Default);
-//}
+    }
 
-//async Task HandleRequest(HttpContext context)
-//{
-//    var response = context.Response;
-//    //response.StatusCode = 404;  // Установка кодов статуса
-//    response.Headers.ContentLanguage = "ru=RU";
-//    response.Headers.ContentType = "text/plain; charset=utf-8"; // для вывода кирилицы
-//    response.Headers.Append("secret-id", "256"); // добавление кастомного заголовка
-//    await response.WriteAsync("моя проэкт лежит на GitHub =)");
-//}
+}
 
-//async Task HandleRequest(HttpContext context)
-//{
-//    var response = context.Response;
-//    response.Headers.ContentType = "text/html; charset=utf-8"; // для вывода HTML
-//    await response.WriteAsync("<h2>Воробушек=)</h2>" +
-//        "<h3>Welcome to web site vorobushek.com </h3>");
-//}
+async Task HandleQueryvar1(HttpContext context)
+{
+    //https://localhost:7094/users?name=Tom&age=37
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.WriteAsync($"<p>Path: {context.Request.Path}</p>" +
+        $"<p>QueryString: {context.Request.QueryString}</p>");
+}
 
-//async Task HandleResponse(HttpContext context)
-//{
-//    var response = context.Response;
-//    response.ContentType = "text/html; charset=utf-8";
-//    var stringBuilder = new System.Text.StringBuilder("<table>");
+async Task HandleQueryvar2(HttpContext context)
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    System.Text.StringBuilder stringB = new System.Text.StringBuilder
+        ("<h3>Параметры строки запроса</h3><table>");
+    stringB.Append("<tr><td>Параметр</td><td>Значение</td></tr>");
 
-//    foreach (var header in context.Request.Headers)
-//    {
-//        stringBuilder.Append($"<tr>" +
-//            $"<td>{header.Key}</td>" +
-//            $"<td>{header.Value}</td>" +
-//            $"</tr>");
-//    }
-//    stringBuilder.Append("</table>");
-//    await context.Response.WriteAsync(stringBuilder.ToString());
-//}
-//async Task HandlePathResponse(HttpContext context)
-//{
-//    var response = context.Response;
-//    var path = context.Request.Path;
-//    var now = DateTime.Now;
+    foreach (var item in context.Request.Query)
+    {
+        stringB.Append($"<tr><td>{item.Key}</td><td>{item.Value}</td></tr>");
+    }
+    stringB.Append("<table>");
 
-//    if (path == "/data")
-//    {
-//        await response.WriteAsync($"Date: {now.ToShortDateString()}");
-//    }
-//    else if (path == "/time")
-//    {
-//        await response.WriteAsync($"Time: {now.ToShortTimeString()}");
-//    }
-//    else if (path == "/minute")
-//    {
-//        await response.WriteAsync($"Minute: {now.ToString()}");
-//    }
-//    else
-//    {
-//        await response.WriteAsync($"\tdata => https://localhost:7094/data \n\thours => https://localhost:7094/time  \n\tminutes => https://localhost:7094/minute");
+    await context.Response.WriteAsync(stringB.ToString());
+}
 
-//    }
+async Task HandleQueryvar3(HttpContext context)
+{
+    //https://localhost:7094/users?name=Tom&age=37 можно вытащить из словаря Query
 
-//}
+    string name = context.Request.Query["name"];
+    string age = context.Request.Query["age"];
+    await context.Response.WriteAsync($"{name} - {age}");
+}
 
-//async Task HandleQueryvar1(HttpContext context)
-//{
-//    //https://localhost:7094/users?name=Tom&age=37
-//    context.Response.ContentType = "text/html; charset=utf-8";
-//    await context.Response.WriteAsync($"<p>Path: {context.Request.Path}</p>" +
-//        $"<p>QueryString: {context.Request.QueryString}</p>");
-//}
-//async Task HandleQueryvar2(HttpContext context)
-//{
-//    context.Response.ContentType = "text/html; charset=utf-8";
-//    System.Text.StringBuilder stringB = new System.Text.StringBuilder
-//        ("<h3>Параметры строки запроса</h3><table>");
-//    stringB.Append("<tr><td>Параметр</td><td>Значение</td></tr>");
-//    foreach (var item in context.Request.Query)
-//    {
-//        stringB.Append($"<tr><td>{item.Key}</td><td>{item.Value}</td></tr>");
-//    }
-//    stringB.Append("<table>");
-//    await context.Response.WriteAsync(stringB.ToString());
-
-//}
-
-//async Task HandleQueryvar3(HttpContext context)
-//{
-////https://localhost:7094/users?name=Tom&age=37 можно вытащить из словаря Query
-
-//    string name = context.Request.Query["name"];
-//    string age = context.Request.Query["age"];
-//    await context.Response.WriteAsync($"{name} - {age}");
-//}
-
+#endregion
---
+++ Solution/Solution.BAseData.SQlite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Solution.BAseData.SQlite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=usersdata.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = "CREATE TABLE Users(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Name TEXT NOT NULL, Age INTEGER NOT NULL)";
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Таблица Users создана");
+            }
+            Console.Read();
+        }
+    }
+}
